b6ffa60a069d5c0e697d361a097c8f417a9e9b99
Ninetta11 <ninawelsh1@outlook.com>
2020-11-23 20:36:10 +1100

    Form submit api post completed & html text content

--- public/assets/js/post.js
+++ public/assets/js/post.js
@@ -1,18 +1,55 @@
 var db = require("../../models");
 
-// on load of page
-$(document).ready(function () {
+var taskTitleInput = $("#inputTaskTitle");
+var taskDescriptionInput = $("#");
+var categoryInput = $("#categories");
 
-    const categoriesListEl = $("#categories");
+// // on load of page
+// $(document).ready(function () {
 
-    // returns all categories from database
-    db.Category.findAll({
-        attributes: name
-    }).then(function (categories) {
-        for (var i = 0; i < categories.length; i++) {
-            let listOptionEl = `<option>${categories[i]}</option>`;
-            categoriesListEl.append(listOptionEl);
-        }
-    })
+//     const categoriesListEl = $("#categories");
+
+//     // returns all categories from database
+//     db.Category.findAll({
+//         attributes: name
+//     }).then(function (categories) {
+//         for (var i = 0; i < categories.length; i++) {
+//             let listOptionEl = `<option>${categories[i]}</option>`;
+//             categoriesListEl.append(listOptionEl);
+//         }
+//     })
+// })
+
+// on click of post button on form
+$(".post-form").on("submit", function (event) {
+    event.preventDefault();
+
+    // saves data inputs on form
+    let taskData = {
+        title = taskTitleInput.val().trim(),
+        description = taskDescriptionInput.val().trim(),
+        category = categoryInput.val()
+    };
+
+    // if any of the form inputs are blank, nothing happens
+    if (!taskData.title || !taskData.description || !taskData.category) {
+        return;
+    }
+
+    addTask(taskData.title, taskData.description, taskData.category);
+    taskTitleInput.val("");
+    taskDescriptionInput.val("");
+    category.val("");
 })
 
+// posts the entered data to the post route
+function addTask(taskTitle, taskDescription, taskCategory) {
+    $.post("/api/post", {
+        title = taskTitle,
+        description = taskDescription,
+        category = taskCategory
+    }).then(function (data) {
+        // this bit is for the login section once we have that 
+        //window.location.replace("members");
+    })
+}
