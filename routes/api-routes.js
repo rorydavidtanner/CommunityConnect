2059a67c045998de865445a69dc09a7b87a6f447
timsilby <69242373+timsilby@users.noreply.github.com>
2020-11-24 06:08:50 +1000

    Added routes for adding/deleting/editing/fetching tasks and fetching categories.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,69 @@
+const db = require('../models');
 
+module.exports = function (app) {
+  // Route to get all unassigned tasks.
+  app.get('/api/tasks', (req, res) => {
+    db.Task.findAll({
+      include: db.Category,
+      where: {
+        isAssigned: 0,
+      },
+    }).then((dbTask) => {
+      res.json(dbTask);
+    });
+  });
 
+  // Route to add a task.
+  app.post('/api/tasks', function (req, res) {
+    console.log(req.body);
+    // res.end();
+    db.Task.create({
+      title: req.body.title,
+      CategoryId: req.body.category,
+      description: req.body.description,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }).then(function (dbTask) {
+      console.log(dbTask);
+      res.json(dbTask);
+    });
+  });
 
+  // Route to delete a task.
+  app.delete('/api/tasks/:id', function (req, res) {
+    db.Task.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbTask) {
+      res.json(dbTask);
+    });
+  });
+
+  // Route to edit a task.
+  app.put('/api/tasks/:id', function (req, res) {
+    console.log(req.body);
+    db.Task.update({
+      title: req.body.title,
+      CategoryId: req.body.category,
+      description: req.body.description,
+      updatedAt: new Date()
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbTask) {
+      res.json(dbTask);
+    })
+  });
+
+  // Route to get categories.
+  app.get('/api/categories', (req, res) => {
+    db.Category.findAll({}).then(function (dbCategory) {
+      res.json(dbCategory);
+    });
+  });
+};
 
 // // routes for CC
 // const express = require("express");
@@ -16,7 +79,7 @@
 //     task.create([
 //         "owner_id", "assignee_id", "category", "title", "details"
 //     ], [
-//         req.body.owner_id, req.body.assignee_id, req.body.category, req.body.title, req.body.details
+//      req.body.owner_id, req.body.assignee_id, req.body.category, req.body.title, req.body.details
 //     ], function (result) {
 //         res.json({ id: result.insertId });
 //     });
@@ -64,4 +127,4 @@
 // });
 
 // Export routes for server.js to use.
-module.exports = router;
+// module.exports = router;
