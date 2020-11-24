9723ecdc8f05720af8a6d7664d8a159493f113a1
timsilby <69242373+timsilby@users.noreply.github.com>
2020-11-24 21:40:57 +1000

    Updated route for /browse to get tasks and render with handlebars. Added some additional handlebars configuration in server.js.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const db = require('../models');
 
 module.exports = function (app) {
   app.get('/', function (req, res) {
@@ -11,9 +12,13 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, '../public/post.html'));
   });
   app.get('/browse', function (req, res) {
-    res.render('browse', data);
+    db.Task.findAll({
+      include: db.Category,
+      where: {
+        isAssigned: 0,
+      },
+    }).then(function (dbTask) {
+      res.render('tasks', { task: dbTask });
+    });
   });
-  app.get('/tasks', function (req, res) {
-    res.render('tasks');
-  });
-};
+}
