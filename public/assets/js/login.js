9697ac9fca5b7cfeeed5a15dce4f7321d9aa7dd6
timsilby <69242373+timsilby@users.noreply.github.com>
2020-11-25 20:55:11 +1000

    Added code to handle form submission on the login page.

---
+++ public/assets/js/login.js
@@ -0,0 +1,34 @@
+// Handler for form submit
+$('#loginForm').on('submit', (event) => {
+
+    event.preventDefault();
+
+	// Get the form data
+	const userData = {
+		email: $("#inputEmail").val().trim(),
+		password: $("#inputPassword").val()
+	}
+
+	// Call the loginUser function and reset the form.
+	loginUser(userData);
+	$('#loginForm').trigger("reset");
+
+});
+
+// Function to login the user. If successful, redirects to the members page, otherwise
+// displays an error.
+function loginUser({ email, password }) {
+
+    $.post('/api/login', {
+        email: email,
+        password: password,
+    })
+        .then(() => {
+            toastMessage('success', `Login successful`);
+            window.location.replace('/');
+        })
+        .catch((err) => {
+            toastMessage('error', err.responseText);
+		});
+
+}
