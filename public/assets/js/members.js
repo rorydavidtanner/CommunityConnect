272018ed0ed3de6aad94433d7ca220fea3cbe6c7
timsilby <69242373+timsilby@users.noreply.github.com>
2020-11-26 09:03:05 +1000

    Updated nav link to point the username link to mytasks.

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -12,7 +12,7 @@ $.get('/api/user_data').then(function (data) {
     if (data.id) {
         navItems = `
         ${navBtns}
-        <a href="/members" class="nav-link">${data.firstName} ${data.lastName}</a>
+        <a href="/mytasks" class="nav-link">${data.firstName} ${data.lastName}</a>
         <a href="/logout" class="nav-link">Logout</a>`;
     }
     else {
@@ -24,44 +24,3 @@ $.get('/api/user_data').then(function (data) {
     $("#navList").html(navItems);
 
 });
-
-// Create global functions for toast messages.
-window.toastTemplate = (alertType, message) => {
-    let title = 'Message';
-    let alertTypeColour = '#007aff';
-    switch (alertType) {
-        case 'error':
-            title = 'Error';
-            alertTypeColour = '#dc3545';
-            break;
-        case 'success':
-            title = 'Success';
-            alertTypeColour = '#28a745';
-            break;
-        default:
-            break;
-    }
-    return `
-<div aria-live="polite" aria-atomic="true" style="position: relative;">
-    <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true" style="position: absolute; top: 20px; right: 20px;">
-        <div class="toast-header" style="min-width: 250px;">
-            <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="${alertTypeColour}"></rect></svg>
-            <strong class="mr-auto">${title}</strong>
-            <small class="text-muted">1 sec ago</small>
-            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="toast-body">
-            ${message}
-        </div>
-    </div>
-</div>
-`;
-};
-
-window.toastMessage = (type, message) => {
-    const $alert = $('#alert');
-    const output = toastTemplate(type, message);
-    $alert.html(output);
-};
