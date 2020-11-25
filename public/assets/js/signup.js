0fb5b06206e78fb27dacb6fd56761a70c16e927c
timsilby <69242373+timsilby@users.noreply.github.com>
2020-11-25 13:58:04 +1000

    Added code to add new user to database on signup form submit.

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,19 +1,21 @@
 // On submit of signup form
 $("#signupForm").on("submit", function (event) {
-    event.preventDefault();
+	event.preventDefault();
+
+	console.log("button clicked");
 
     // Get the form data
     const userData = {
-        email: $('#inputTaskTitle').val().trim(),
-        password: $('#inputTaskDescription').val().trim(),
-        firstName: $('#inputTaskDescription').val().trim(),
-        lastName: $('#inputTaskDescription').val().trim(),
-        phone: $('#inputTaskDescription').val().trim(),
-        address: $('#inputTaskDescription').val().trim()
+        email: $('#inputEmail').val().trim(),
+        password: $('#inputPassword').val().trim(),
+        firstName: $('#inputFirstName').val().trim(),
+        lastName: $('#inputLastName').val().trim(),
+        phone: $('#inputPhone').val().trim(),
+        address: $('#inputAddress').val().trim()
     }
 
     // Make a post request to add the new user to the database.
-    $.post('/api/tasks', taskData).then(function (data) {
+    $.post('/api/users', userData).then(function (data) {
         // this bit is for the login section once we have that
         // window.location.replace("members");
     });
